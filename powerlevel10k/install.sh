378c0b93caaae8029641ac50215758387d1c04a0
j8seangel <jseangel@gmail.com>
2020-01-27 01:58:19 +0100

    install powerlevel10k theme

--- powerlevel10k/install.sh
+++ powerlevel10k/install.sh
@@ -4,8 +4,5 @@
 # https://github.com/romkatv/powerlevel10k#installation
 
 # echo "  Powerlevel10k theme."
-# folder=./powerlevel10k/theme
-# rm -rf "$folder"
-# git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $folder
-# echo '\n# Use powerlevel10k theme' >> ~/.zshrc
-# echo 'source ./powerlevel10k/theme/powerlevel10k.zsh-theme' >> ~/.zshrc
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k2
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
