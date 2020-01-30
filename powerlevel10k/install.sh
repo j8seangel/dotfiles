41a14ffb93d8c7f6ea00306f4839a6ea6b21d746
j8seangel <jseangel@gmail.com>
2020-01-30 10:22:28 +0100

    clone powerlevel10k

--- powerlevel10k/install.sh
+++ powerlevel10k/install.sh
@@ -4,5 +4,5 @@
 # https://github.com/romkatv/powerlevel10k#installation
 
 # echo "  Powerlevel10k theme."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k2
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
