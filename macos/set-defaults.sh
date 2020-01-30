c6cc0b36734c1530a9428ff2b5b72be33dbce7f8
j8seangel <jseangel@gmail.com>
2020-01-30 10:22:35 +0100

    fix iTerm2 folder

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -80,7 +80,7 @@
 defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
 # Specify the preferences directory
-defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles/iTerm2"
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/iTerm2"
 # Tell iTerm2 to use the custom preferences in the directory
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
