7ed4b9ac465446b687ef96a53cc2bf69171e8fa8
j8seangel <jseangel@gmail.com>
2023-12-04 08:10:53 +0100

    fix python server

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 alias cleanup="find . -type f -name '*.DS_Store' -ls -delete"
-alias server="python -m SimpleHTTPServer 8000"
+alias server="python -m http.server 8000"
 alias update='sudo softwareupdate -i -a; brew -v update; brew upgrade; brew cleanup; brew prune'
 alias bye="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 
