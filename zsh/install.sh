2a641439d4b60aefc39249a519114d041b3c2f3f
j8seangel <jseangel@gmail.com>
2025-02-09 11:48:24 +0100

    fix autocomplete

---
+++ zsh/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#
+# Node
+#
+
+# Install zsh plugins
+then
+  echo "  Installing zsh-syntax-highlighting for you."
+  brew install zsh-syntax-highlighting
+
+  echo "  Installing zsh-autosuggestions for you."
+  brew install zsh-autosuggestions
+fi
+
+exit 0
